2129c07c0f26292f58ee2a12f9dedc26e74bdb5a
Nicholas J. Parks <nparks@kenzan.com>
2017-07-18 13:31:04 -0400

    misc formatting

--- gcp/scripts/k8_oauth.sh
+++ gcp/scripts/k8_oauth.sh
@@ -1,6 +1,6 @@
 
 ##########################
-# Kenzan LLC Credentials Bucket
+# Kenzan LLC -> Credentials Bucket
 # 
 ## Did you create GKE first?
 #
@@ -10,12 +10,9 @@
 
 
 #Information about the cluster you created from the console
-GKE_URL=https://GKE_IP_FROM_CONSOLE
-GKE_PASSWORD=[THE PASSWORD FROM THE CONSOLE]
-
-
-#when you created to consent screen and the new set of oauth credentials you got two important values
-
+GKE_URL="https://GKE_IP_FROM_CONSOLE"
+GKE_PASSWORD="[THE PASSWORD FROM THE CONSOLE]"
+#when you created to consent screen and the new set of oauth credentials you got two important values:
 OAUTH2_CLIENT_SECRET=X
 OAUTH2_CLIENT_ID=y
 
