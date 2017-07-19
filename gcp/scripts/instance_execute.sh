ea16ab66863ac25607152c49e777ae2f8668044a
Nicholas J. Parks <nparks@kenzan.com>
2017-07-19 07:52:55 -0400

    terraform entry script to make some stopping api calls...need to have terraform perform this operations or some other solution to perform these calls

--- gcp/scripts/instance_execute.sh
+++ gcp/scripts/instance_execute.sh
@@ -10,25 +10,41 @@
 ##########################
 ###
 
+TOOLS_SERVICE_ACCOUNT_NAME=super-halyard
+
+
 echo "******************************************"
 echo "=========================================="
 echo " - Starting CD Environment Creation Process -"
 echo "=========================================="
+
+PROJECT_NAME=$(gcloud info --format='value(config.project)')
+
+
 
 ### We need to get kubectl instead of having gloud install it for us
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+
 #### get halyard
 curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/stable/InstallHalyard.sh
-sudo bash InstallHalyard.sh
+sudo bash InstallHalyard.sh -y
 
 hal -v 
 
 ### get git
 sudo apt-get install git -y
 #### do some API enable
+
+
+echo "=====  If asked a question say yes (y) ====="
+
+SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:$TOOLS_SERVICE_ACCOUNT_NAME" --format='value(email)')
+
+THROW_AWAY_VAR=$(gcloud projects add-iam-policy-binding $PROJECT_NAME --role roles/storage.admin --member serviceAccount:$SA_EMAIL)
+
 
 echo "=========================================="
 echo " - Configuration Set-up Complete -"
