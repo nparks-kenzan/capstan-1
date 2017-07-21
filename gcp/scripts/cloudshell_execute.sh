be6d8f28109cd3a6189ed24d264ec5498db187a7
Nicholas J. Parks <nparks@kenzan.com>
2017-07-21 16:32:51 -0400

    updates to project verification script

--- gcp/scripts/cloudshell_execute.sh
+++ gcp/scripts/cloudshell_execute.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ##########################
-# Kenzan LLC Script Executed by TerraForm on GCP Instance
+# Kenzan LLC Script to Validate Project settings
 # 
 # Do you have a powerfull enough service account to do this stuff?
 # THis is to run from the google cloud shell
@@ -9,16 +9,16 @@
 # nparks@kenzan.com
 ##########################
 ###
-source ./env.sh
+#source ./env.sh
 #
-
+TOOLS_SERVICE_ACCOUNT_NAME="the display name of the service account you created for terraform"
 
 PROJECT_NAME=$(gcloud info --format='value(config.project)')
-gcloud config set compute/zone $ZONE
+
 
 SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:$TOOLS_SERVICE_ACCOUNT_NAME" --format='value(email)')
 
-gcloud compute --project $PROJECT_NAME instances create "halyard-tools" --zone $ZONE --machine-type "n1-standard-1" --subnet "default" --maintenance-policy "MIGRATE" --service-account $SA_EMAIL --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --image "ubuntu-1404-trusty-v20170703" --image-project "ubuntu-os-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "halyard-tools"
+THROW_AWAY_VAR=$(gcloud projects add-iam-policy-binding $PROJECT_NAME --role roles/storage.admin --member serviceAccount:$SA_EMAIL)
 
 
 
