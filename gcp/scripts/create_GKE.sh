cdf6d996ddc67d43dd8b6672ffb61644b1b2afda
Nicholas J. Parks <nparks@kenzan.com>
2017-08-20 20:29:15 -0400

    remove unneeded file and do #20

--- gcp/scripts/create_GKE.sh
+++ gcp/scripts/create_GKE.sh
@@ -20,7 +20,7 @@ PROJECT_NAME=$(gcloud info --format='value(config.project)')
 gcloud config set compute/zone $ZONE
 # Create the Actual GKE cluster
 
-gcloud container --project $PROJECT_NAME clusters create $CLUSTER_NAME --zone $ZONE --username="admin" --cluster-version "1.6.4" --machine-type "n1-standard-1" --image-type "COS" --disk-size "100" --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/cloud-platform","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/source.read_write","https://www.googleapis.com/auth/projecthosting,storage-rw" --num-nodes $NODE_PER_ZONE --network "default" --enable-cloud-logging --no-enable-cloud-monitoring $OTHER_ZONES --enable-legacy-authorization
+gcloud container --project $PROJECT_NAME clusters create $CLUSTER_NAME --zone $ZONE --username="admin" --cluster-version "1.6.4" --machine-type "n1-standard-2" --image-type "COS" --disk-size "100" --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/cloud-platform","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/source.read_write","https://www.googleapis.com/auth/projecthosting,storage-rw" --num-nodes $NODE_PER_ZONE --network "default" --enable-cloud-logging --no-enable-cloud-monitoring $OTHER_ZONES --enable-legacy-authorization
 
 
 #make kubectl happy for later usage
@@ -28,6 +28,8 @@ gcloud config set container/use_client_certificate true
 gcloud container clusters get-credentials $CLUSTER_NAME
 
 kubectl cluster-info 
+
+helm init
 
 ## this is where we need to do a watch command to see what the status of the cluster is
 sleep 60
