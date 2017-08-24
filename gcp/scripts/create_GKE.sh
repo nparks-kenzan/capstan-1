3b13fc57a58fe0f65e2af438b312e9c21fe1f059
Nicholas J. Parks <nparks@kenzan.com>
2017-08-24 15:39:12 -0400

    Changes per @JinpaLhawang

--- gcp/scripts/create_GKE.sh
+++ gcp/scripts/create_GKE.sh
@@ -18,8 +18,18 @@ echo "=========================================="
 
 PROJECT_NAME=$(gcloud info --format='value(config.project)')
 gcloud config set compute/zone $ZONE
-# Create the Actual GKE cluster
 
+# enable commands
+
+echo ">>>>> Enable gcloud API needs"
+
+gcloud service-management enable iam.googleapis.com
+gcloud service-management enable cloudresourcemanager.googleapis.com
+gcloud service-management enable container.googleapis.com
+
+
+# Create the Actual GKE cluster
+echo ">>>>> Create Cluster"
 gcloud container --project $PROJECT_NAME clusters create $CLUSTER_NAME --zone $ZONE --username="admin" --cluster-version "1.6.4" --machine-type "n1-standard-2" --image-type "COS" --disk-size "100" --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/cloud-platform","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/source.read_write","https://www.googleapis.com/auth/projecthosting,storage-rw" --num-nodes $NODE_PER_ZONE --network "default" --enable-cloud-logging --no-enable-cloud-monitoring $OTHER_ZONES --enable-legacy-authorization
 
 
