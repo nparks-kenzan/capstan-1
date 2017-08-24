ea437ffc7afe5064d928f506bf7c8320b08ceaa7
Nicholas J. Parks <nparks@kenzan.com>
2017-08-24 14:23:21 -0400

    display credentials and IP

--- gcp/scripts/create_jenkins.sh
+++ gcp/scripts/create_jenkins.sh
@@ -17,15 +17,17 @@ echo "=========================================="
 echo " - Let's Get this Jenkins Thing Together -"
 echo "=========================================="
 
-
-
+#expecting tiller is in the environment
+#the wait flag means everything should be done...the replace flag means stop over existing
 helm install stable/jenkins --name $JENKINS_HELM_RELEASENAME --namespace $JENKINSNS --values $PWD/jenkins_helm.yml --replace --wait
 
 
+JENKINS_ADDRESS=$(kubectl get svc --namespace jenkins ci-jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
+JENKINS_PW=`printf $(kubectl get secret --namespace $JENKINS_NAMESPACE $JENKINS_HELM_RELEASENAME-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo`
 
 
 echo "=========================================="
-echo " - Jenkins with $PASSWORD at $JENKINS_ADDRESS -"
+echo " - Jenkins with $JENKINS_PW at $JENKINS_ADDRESS -"
 echo "=========================================="
 echo "******************************************"
 
