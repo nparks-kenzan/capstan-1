f415711bf7cbdc4910227f8a442ae8e651728ef2
Daniel Williams <dwilliams@kenzan.com>
2018-01-16 09:42:03 -0700

    Updating GKE version.

--- gcp/scripts/create_GKE.sh
+++ gcp/scripts/create_GKE.sh
@@ -2,7 +2,7 @@
 
 ##########################
 # Kenzan LLC Create GKE for SpinnakerJenkins
-# 
+#
 ## Can your GCP Service Account do this?
 #
 # nparks@kenzan.com
@@ -30,14 +30,16 @@ gcloud service-management enable container.googleapis.com
 
 # Create the Actual GKE cluster
 echo ">>>>> Create Cluster"
-gcloud container --project $PROJECT_NAME clusters create $CLUSTER_NAME --zone $ZONE --username="admin" --cluster-version "1.7.3" --machine-type "n1-standard-2" --image-type "COS" --disk-size "100" --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/cloud-platform","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/source.read_write","https://www.googleapis.com/auth/projecthosting,storage-rw" --num-nodes $NODE_PER_ZONE --network "default" --enable-cloud-logging --enable-cloud-monitoring $OTHER_ZONES --enable-legacy-authorization
+GKE_CLUSTER_VERSION="1.8.6-gke0"
+GKE_MACHINE_TYPE="n1-standard-2"
+gcloud container --project $PROJECT_NAME clusters create $CLUSTER_NAME --zone $ZONE --username="admin" --cluster-version $GKE_CLUSTER_VERSION --machine-type $GKE_MACHINE_TYPE --image-type "COS" --disk-size "100" --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/cloud-platform","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/source.read_write","https://www.googleapis.com/auth/projecthosting,storage-rw" --num-nodes $NODE_PER_ZONE --network "default" --enable-cloud-logging --enable-cloud-monitoring $OTHER_ZONES --enable-legacy-authorization
 
 
 #make kubectl happy for later usage
 gcloud config set container/use_client_certificate true
 gcloud container clusters get-credentials $CLUSTER_NAME
 
-kubectl cluster-info 
+kubectl cluster-info
 echo ">>>>> Helm Init"
 helm init
 
