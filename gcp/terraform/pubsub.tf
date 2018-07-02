4c597c15092f96e6d5f25cbc187da0dccf881b83
Nicholas J. Parks <nparks@kenzan.com>
2018-07-02 09:51:33 -0400

    hopefully the correct way to specify a topic in terraform

--- gcp/terraform/pubsub.tf
+++ gcp/terraform/pubsub.tf
@@ -1,7 +1,7 @@
 # This is the topic and subscription that spinnaker will use to hear things from GCR
 
 resource "google_pubsub_topic" "gcr_event_stream" {
-  name = "projects/${var.gcp_project_id}/topics/gcr"
+  name = "gcr"
 }
 
 resource "google_pubsub_subscription" "spinnaker_subscription" {
