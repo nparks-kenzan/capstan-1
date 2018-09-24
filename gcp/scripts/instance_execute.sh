084eed4511cee68eec63a0427da5d37199298339
Nicholas J. Parks <nparks@kenzan.com>
2018-09-24 11:34:04 -0400

    need to touch that configuration file for spin

--- gcp/scripts/instance_execute.sh
+++ gcp/scripts/instance_execute.sh
@@ -61,7 +61,8 @@ echo ">>>>> get spin"
 curl -LO https://storage.googleapis.com/spinnaker-artifacts/spin/$(curl -s https://storage.googleapis.com/spinnaker-artifacts/spin/latest)/linux/amd64/spin
 chmod +x spin
 sudo mv spin /usr/local/bin/spin
-
+mkdir ~/.spin
+touch ~/.spin/config
 #### We need expect also
 #sudo apt-get install expect -y
 
