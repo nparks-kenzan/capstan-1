af3b3df16259bd23bfe96dd5a14786bc457b3482
Nicholas J. Parks <nparks@kenzan.com>
2019-08-07 12:25:12 -0400

    use a less big hammer means to make these IAM additions

--- gcp/terraform/iam.tf
+++ gcp/terraform/iam.tf
@@ -8,57 +8,39 @@ resource "google_service_account" "spinnaker" {
   display_name = "${var.spinnaker_service_account_name}"
 }
 
-resource "google_project_iam_policy" "project" {
-  project     = "${var.gcp_project_id}"
-  policy_data = "${data.google_iam_policy.admin.policy_data}"
+
+resource "google_project_iam_member" "spin_pubsub_iammember" {
+  project = "${var.gcp_project_id}"
+  role    = "roles/pubsub.subscriber"
+  member  = "serviceAccount:${google_service_account.spinnaker.email}"
 }
 
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/owner"
+resource "google_project_iam_member" "spin_computeview_iammember" {
+  project = "${var.gcp_project_id}"
+  role    = "roles/compute.viewer"
+  member  = "serviceAccount:${google_service_account.spinnaker.email}"
+}
 
-    members = [
-      "serviceAccount:${google_service_account.halyard_toolsacct.email}",
-    ]
-  }
+resource "google_project_iam_member" "spin_metricwrite_iammember" {
+  project = "${var.gcp_project_id}"
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.spinnaker.email}"
+}
 
-  binding {
-    role = "roles/storage.admin"
+resource "google_project_iam_member" "spin_monitorview_iammember" {
+  project = "${var.gcp_project_id}"
+  role    = "roles/monitoring.viewer"
+  member  = "serviceAccount:${google_service_account.spinnaker.email}"
+}
 
-    members = [
-      "serviceAccount:${google_service_account.spinnaker.email}",
-    ]
-  }
+resource "google_project_iam_member" "spin_storageadmin_iammember" {
+  project = "${var.gcp_project_id}"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.spinnaker.email}"
+}
 
-  binding {
-    role = "roles/monitoring.viewer"
-
-    members = [
-      "serviceAccount:${google_service_account.spinnaker.email}",
-    ]
-  }
-
-  binding {
-    role = "roles/monitoring.metricWriter"
-
-    members = [
-      "serviceAccount:${google_service_account.spinnaker.email}",
-    ]
-  }
-
-  binding {
-    role = "roles/compute.viewer"
-
-    members = [
-      "serviceAccount:${google_service_account.spinnaker.email}",
-    ]
-  }
-
-  binding {
-    role = "roles/pubsub.subscriber"
-
-    members = [
-      "serviceAccount:${google_service_account.spinnaker.email}",
-    ]
-  }
+resource "google_project_iam_member" "tools_owneradmin_iammember" {
+  project = "${var.gcp_project_id}"
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.halyard_toolsacct.email}"
 }
